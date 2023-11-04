9ac1a77c1c703381262f436b7f40af7ca2551ac6
Lance McMahon <mcmahon.lancej@gmail.com>
2023-11-04 02:03:31 -0400

    Adding source code

---
+++ generic/generic.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // Optimize input/output
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // YOUR CODE HERE
+
+
+
+    /* CHEAT SHEET:
+
+    - Read successive space separated integer variables:
+
+        int a, b;
+        cin >> a >> b;
+
+    - Read n lines of input where line n = "<char> <int>":
+
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            char a;
+            int x;
+            cin >> a >> x;
+        }
+
+    - Read 3 lines: 1: int, 2: int, 3: string:
+
+        int a, b;
+        string s;
+        cin >> a >> b >> s;
+
+    - Read entire input as a string:
+
+        istreambuf_iterator<char> begin(cin), end;
+        string s(begin, end);
+
+    - Convert int to string:
+
+        int a = 0;
+        ostringstream oss;
+        oss << a;
+        string result = oss.str();    
+    */
+   
+    return 0;
+}
